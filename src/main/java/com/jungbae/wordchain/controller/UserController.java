fb32c571d41f29ae6f6b56a334513d210b421ab9
son12039 <sonson12039@gmail.com>
2025-05-05 17:52:23 +0900

    1

--- src/main/java/com/jungbae/wordchain/controller/UserController.java
+++ src/main/java/com/jungbae/wordchain/controller/UserController.java
@@ -10,10 +10,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
   
 
 @RestController
+@CrossOrigin("*")
 public class UserController {
 
 	@Autowired
