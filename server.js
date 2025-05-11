a4a167d26346b7f006b3dd8e7565b6ddcc093f96
son12039 <sonson12039@gmail.com>
2025-05-11 20:34:54 +0900

    포트를 로컬로 해놨었네

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import http from "http";
 import { createSocket } from "./socket.js";
 import { createDBConnection } from "./mysql.js";
 const app = express();
-const port = 56516;
+const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 const server = http.createServer(app);
