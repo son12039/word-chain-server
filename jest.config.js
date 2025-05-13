129e5f7e7b602b1fefd0c451e01fea6283e071d3
son12039 <sonson12039@gmail.com>
2025-05-14 08:06:47 +0900

    1

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest", // Babel을 사용하여 .js, .jsx, .ts, .tsx 파일 변환
-  },
-  testEnvironment: "node", // 테스트 환경을 Node.js로 설정
-};
